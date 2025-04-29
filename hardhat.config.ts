f329fac35402e9437df183a4f8b1ecb43aac5e9a
coddery888 <159686575+coddery888@users.noreply.github.com>
2025-04-29 17:26:28 +0800

    able to test via forking chain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,12 @@ import "dotenv/config";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
+    hardhat: {
+      forking: {
+        url: process.env.OP_BNB_TESTNET_URL!,
+        blockNumber: 60000000,
+      },
+    },
     opBnbMainnet: {
       url: process.env.OP_BNB_MAINNET_URL,
       accounts: [process.env.PRIVATE_KEY!],
