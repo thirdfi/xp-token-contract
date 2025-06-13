3cdf0b56e3112e2d20d997f996b4942dcd354754
siewchongche <siew@daoventures.co>
2025-06-13 22:08:50 +0800

    upgrade MAIGA_XP contract for signature based mint

--- contracts/MAIGA_XP.sol
+++ contracts/MAIGA_XP.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract MAIGA_XP is
     Initializable,
@@ -12,18 +14,54 @@ contract MAIGA_XP is
     OwnableUpgradeable,
     ERC20Upgradeable
 {
+    using MessageHashUtils for bytes32;
+
     constructor() {
         _disableInitializers();
     }
+
+    address public signer;
+    mapping(bytes32 => bool) public usedSignatures;
+    mapping(address => uint256) public totalMinted;
 
     function initialize() public initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         __ERC20_init("MAIGA XP", "MAIGA_XP");
+        signer = msg.sender;
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function mint(uint256 amount, bytes calldata signature) public {
+        require(
+            !usedSignatures[keccak256(signature)],
+            "Signature already used"
+        );
+        usedSignatures[keccak256(signature)] = true;
+
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                amount,
+                totalMinted[msg.sender],
+                block.chainid
+            )
+        );
+        address recoveredSigner = ECDSA.recover(
+            messageHash.toEthSignedMessageHash(),
+            signature
+        );
+        require(recoveredSigner == signer, "Invalid signature");
+
+        totalMinted[msg.sender] += amount;
+        _mint(msg.sender, amount);
+    }
+
+    function setSigner(address _signer) external onlyOwner {
+        signer = _signer;
     }
 
     function _update(
